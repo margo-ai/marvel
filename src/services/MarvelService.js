a29baf44d38568abd6db078913aa674fc986b221
Margo <plokhova.margarita@yandex.ru>
2022-10-05 19:46:49 +0300

    Fixed bug with method name

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -27,7 +27,7 @@ const useMarvelService = () => {
         return res.data.results.map(_transformComics);
     }
 
-    const getComics = async (id) => {
+    const getComic = async (id) => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
         return _transformComics(res.data.results[0]);
     }
@@ -56,7 +56,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacterByName, getCharacter, clearError, getAllComics, getComics}
+    return {loading, error, getAllCharacters, getCharacterByName, getCharacter, clearError, getAllComics, getComic}
 
 }
 
