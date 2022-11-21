ffbcebce6e66e2efa2c1c2a1f9eb1ee96aefa465
Margo <plokhova.margarita@yandex.ru>
2022-11-21 20:35:02 +0300

    added react-helmet and changed title of each page

--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,10 +1,18 @@
 import './singleCharacterLayout.min.css';
+import { Helmet } from 'react-helmet';
 
 const SingleCharacterLayout = ({data}) => {
     const {name, description, thumbnail} = data;
 
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`Page about ${name}`}
+                />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__char-img" />
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
